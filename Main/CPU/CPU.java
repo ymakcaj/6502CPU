5481729b8d9c5ad892ccead4f9dd1ce9a45366e6
Jack Amy <jack.amy@moneyfarm.com>
2023-08-22 21:34:51 +0100

    first class finished - the CPU

--- Main/CPU/CPU.java
+++ Main/CPU/CPU.java
@@ -13,8 +13,17 @@ public class CPU {
 	public Byte Y;
 
 	public Byte C = new Byte(1);
+	public Byte Z = new Byte(1);
+	public Byte I = new Byte(1);
+	public Byte D = new Byte(1);
+	public Byte B = new Byte(1);
+	public Byte V = new Byte(1);
+	public Byte N = new Byte(1);
 
-	
-	
+	void reset() {
+		programCounter = 0xFFFC;
+		stackPOinter = 0x00FF;
+		D = Byte(0);
+	}
 	
 }
